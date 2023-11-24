9071a609b63f1a13128a17a577126037c5160eb3
mikhails23 <144317419+mikhails23@users.noreply.github.com>
2023-11-24 20:46:58 +0700

    Base gameplay
    Paddles control with (W, S) and (I, K) and ball bounce physics

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    public int speed;
+    public KeyCode upKey;
+    public KeyCode downKey;
+    private Rigidbody2D rig;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Gerakan dari input
+        MoveObject(GetInput());
+    }
+
+    private Vector2 GetInput() { 
+        // Ambil input
+        if (Input.GetKey(upKey)) {
+            // Gerakan ke atas
+            return Vector2.up * speed;
+        }
+        else if (Input.GetKey(downKey)) {
+            // Gerakan ke bawah
+            return Vector2.down * speed;            
+        }
+
+        // Ketika tidak ada input maka diam
+        return Vector2.zero;
+    }
+
+    private void MoveObject(Vector2 movement) {
+        rig.velocity = movement;
+    }
+}
